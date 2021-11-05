d90229be46a2e889be611d670451d904f79058bf
solregem <greenberg04@gmail.com>
2021-11-04 23:29:02 -0700

    a

--- csmathlib/structures.cs
+++ csmathlib/structures.cs
@@ -1,5 +1,6 @@
 ﻿namespace csmathlib;
 
+using System.Collections.Generic;
 using static Statics;
 
 
@@ -27,33 +28,50 @@ public struct Complex
 public class Function
 {
     public Function( Func<double, Complex> f ) => this.f = f;
-    private Func<double, Complex> f;
+    protected Func<double, Complex> f;
 
     public Complex this[ double x ]
     {
         get => f( x );
     }
 
-    public Complex Integral( double b, double a )
+    public Complex Derivative( double x, double dx = .1 )
     {
-        Complex ret = 0;
-        for ( double z = b; z < a; z += dx )
-            ret += f( z ) * dx;
-        return ret;
+        return ( f( x + dx ) - f( x ) ) / dx;
     }
-    public Complex Integral( Complex C )
+    public Function Derivative( double dx = .1 )
     {
-        Complex ret = C;
-        for ( double z = -Infinity; z < Infinity; z += dx )
+        return new Function( x => Derivative( x, dx ) );
+    }
+    public Complex DerivativeN( double x, int N, double dx = .1 )
+    {
+        if ( N == 1 )
+            return Derivative( x, dx );
+        return Derivative( dx ).DerivativeN( x, N - 1, dx );
+    }
+
+    public Polynomial TaylorSeries( int N )
+    {
+        List<(Complex, int)> terms = new();
+        terms.Add( (f( 0 ).a, 0) );
+        for ( int b = 1; b < N; ++b )
         {
-            ret += f( z ) * dx;
+            terms.Add( (DerivativeN( 0, b ) / Factorial( b ), b) );
         }
-        return ret;
+        return new Polynomial( terms.ToArray() );
+    }
+
+    private long Factorial( long N )
+    {
+        if ( N <= 1 )
+            return 1;
+        return N * Factorial( N - 1 );
     }
 
     public Complex Fourier( Complex w, Complex C )
     {
-        return new Function( z => f( z ) * Exp( -2 * Math.PI * i * z * w ) ).Integral( C );
+        Function fn = new Function( z => f( z ) * Exp( -2 * Math.PI * i * z * w ) ).TaylorSeries( 10 ).Integral( C );
+        return fn[ 1000 ] - fn[ -1000 ] + C;
     }
     public Function Fourier( Complex C )
     {
@@ -61,10 +79,52 @@ public class Function
     }
     public Complex InverseFourier( Complex x, Complex C )
     {
-        return new Function( z => f( z ) * Exp( 2 * Math.PI * i * z * x ) ).Integral( C );
+        Function fn = new Function( z => f( z ) * Exp( 2 * Math.PI * i * z * x ) ).TaylorSeries( 10 ).Integral( C );
+        return fn[ 1000 ] - fn[ -1000 ] + C;
     }
     public Function InverseFourier( Complex C )
     {
         return new Function( z => InverseFourier( z, C ) );
     }
+}
+
+public class Polynomial : Function
+{
+    public Polynomial( params (Complex, int)[] terms ) :
+        base( x =>
+        {
+            Complex ret = 0;
+            for ( int a = 0; a < terms.Length; ++a )
+                ret += terms[ a ].Item1 * Math.Pow( x, terms[ a ].Item2 );
+            return ret;
+        } )
+    {
+        this.terms = terms;
+    }
+
+    private (Complex, int)[] terms;
+
+    public Polynomial Integral( Complex C )
+    {
+        List<(Complex, int)> NewTerms = new();
+        for ( int a = 0; a < terms.Length; ++a )
+        {
+            if ( terms[ a ].Item2 != 0 && terms[ a ].Item2 != -1 )
+                NewTerms.Add( (terms[ a ].Item1 / terms[ a ].Item2, terms[ a ].Item2 + 1) );
+        }
+        NewTerms.Add( (C, 0) );
+        return new Polynomial( NewTerms.ToArray() );
+    }
+    public Polynomial Derivative()
+    {
+        List<(Complex, int)> NewTerms = new();
+        for ( int a = 0; a < terms.Length; ++a )
+        {
+            if ( terms[ a ].Item2 != 0 )
+                NewTerms.Add( (terms[ a ].Item1 * terms[ a ].Item2, terms[ a ].Item2 - 1) );
+        }
+        return new Polynomial( NewTerms.ToArray() );
+    }
+
+    
 }
--- csmathlibtest/Program.cs
+++ csmathlibtest/Program.cs
@@ -1,10 +1,10 @@
 ﻿using csmathlib;
 using static csmathlib.Statics;
 
-Function X = new Function( x => x ).Fourier( 0 );
-Function y = new Function( w => X[ w ] / ( 2 * Math.PI * i * w - 1 )).InverseFourier( 0 );
+//Function X = new Function( x => x ).Fourier( 0 );
+Function y = new Function( w => 1 / ( 2 * Math.PI * i * w - 1 )).InverseFourier( 0 );
 
 for ( double x = -20; x < 10; x += .1 )
 {
-    Console.WriteLine( $"{x:F3}: {X[ x ]}" );
+    Console.WriteLine( $"{x:F3}: {y[ x ]}" );
 }
