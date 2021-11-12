6a66cec9674e6556580c5bd632dcd434290bff86
solregem <greenberg04@gmail.com>
2021-11-11 21:47:22 -0800

    a

--- csmathlibtest/Program.cs
+++ csmathlibtest/Program.cs
@@ -9,18 +9,11 @@ using static csmathlib.Statics;
 //  Step 3: solve the original differential equation with x=0
 //  Step 4: add the inverse fourier transform and the solution at x=0 to get a general solution
 //
-//example is shown below
 
-//dy/dx = cos( x ) + y
+Function X = new Function( x => x ).Fourier();
+Function y = new Function( w => X[ w ] / ( 2 * Math.PI * i * w - 1 ) ).InverseFourier();
 
-const double F = 10;
-const double M = 100;
-const double r = -1;
-const double C = 0;
-
-Function x = new Function( t => (F*t + C)/(M + r*t) ).Integral( 0 );
-
-for ( double t = 0; t < 10; t += .5 )
+for ( double t = 0; t < 30; t += .5 )
 {
-    Console.WriteLine( $"{t:F3}: {x[ t ]}" );
+    Console.WriteLine( $"{t:F3}: {y[ t ]}" );
 }
